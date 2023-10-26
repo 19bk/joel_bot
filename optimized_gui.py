f43ff654b1c1e30f747c0c345e17690ae069cc28
19bk <bernardkibathi91@gmail.com>
2023-10-26 21:31:07 +0300

    new file:   optimized_gui.py

---
+++ optimized_gui.py
@@ -0,0 +1,331 @@
+import random
+from PyQt5.QtWidgets import QApplication, QWidget, QHBoxLayout, QVBoxLayout, QPushButton, QTextEdit, QLabel
+from pybit.unified_trading import HTTP
+import ccxt
+from time import sleep
+import sys
+from PyQt5.QtCore import pyqtSignal, QThread, QDateTime, QObject
+from PyQt5.QtCore import QTimer
+
+symbol='TRBUSDT'
+leverage="25"
+take_profit_percentage = 0.5  # Represents 0.5%
+stop_loss_percentage = 0.4   # Represents 0.4%
+
+class PnlThread(QThread):
+    pnl_updated = pyqtSignal(float, float,float, str, str)  # Add a signal for error handling
+
+    def __init__(self, exchange, session, symbol):
+        super().__init__()
+        self.exchange = exchange
+        self.session = session
+        self.symbol = symbol
+
+    def run(self):
+        while True:
+            try:
+                data = self.session.get_positions(category="linear", symbol=self.symbol)
+                # print(data)
+                pnl_value = data['result']['list'][0]['unrealisedPnl']
+                actual_pnl_value = 0
+                if pnl_value:
+                    pnl = round(float(pnl_value), 2)
+                else:
+                    pnl = 0.0
+
+                if actual_pnl_value:
+                    actual_pnl_value = round(float(actual_pnl_value), 2)
+                else:
+                    actual_pnl_value = 0.0
+
+                balance = self.session.get_wallet_balance(accountType="UNIFIED", coin="USDT")
+                account_balance = round(float(balance['result']['list'][0]['coin'][0]['equity']), 2)
+                loc_side = data['result']['list'][0]['side']
+                self.pnl_updated.emit(pnl, actual_pnl_value, account_balance, loc_side, None)  # Emit the updated values
+            except Exception as e:
+                # print(e)
+                self.pnl_updated.emit(0.0, 0.0,0.0, "", str(e))  # Emit an error signal
+
+            self.msleep(50)  # Adjust the sleep interval
+
+class MyApp(QWidget):
+    pnl_updated = pyqtSignal(float, float, str)
+
+    def __init__(self, key, secret, testnet=True):
+        super().__init__()
+        self.initUI()
+        self.session = self.init_session(key, secret, testnet)
+        self.exchange = self.init_exchange(key, secret)
+        self.symbol = symbol
+        self.leverage = leverage
+        self.pnl_thread = None  # Initialize the thread reference
+        self.position_closed = False
+
+
+    def start_pnl_thread(self):
+        self.pnl_thread = PnlThread(self.exchange, self.session, self.symbol)
+        self.pnl_thread.pnl_updated.connect(self.update_pnl_and_balance_ui)
+        self.pnl_thread.start()
+
+    def random_side_chooser(self):
+        return random.choice(['Buy', 'Sell'])
+    
+    def delayed_order(self, chosen_side):
+        if chosen_side == "Sell":
+            self.sell_clicked()
+        else:
+            self.buy_clicked()
+
+    def update_pnl_and_balance_ui(self, pnl, realized_pnl, balance, emitted_side, error_message):
+        # Avoid locking the mutex here
+        side_color = 'green' if emitted_side == "Buy" else 'red'
+        pnl_color = 'green' if pnl >= 0.0 else 'red'
+        true_pnl_color = 'green' if realized_pnl >= 0.0 else 'red'
+        
+
+        self.pos_side.setText(f"<font color='{side_color}'><b>Side:</b> {emitted_side}</font>")
+        self.pnl_label.setText(f"<font color='{pnl_color}'><b>PNL:</b> {pnl}</font>")
+        # self.true_pnl_label.setText(f"<font color='{true_pnl_color}'><b>Actual PNL:</b> {realized_pnl}</font>")
+        self.balance_label.setText(f"Balance: {balance}")
+
+        if error_message:
+            # self.textbox.append(f"update_pnl_and_balance_ui Error: {error_message} /n")
+            pass
+        
+        # print(pnl)
+        # if pnl >= 20 and not self.position_closed:
+        #     self.position_closed = True
+            
+        #     self.close_position_clicked()
+        #     self.textbox.append(f"Position closed with Profit {pnl}")
+        #     chosen_side = self.random_side_chooser()
+        #     QTimer.singleShot(10000, lambda: self.delayed_order(chosen_side))  # 10 seconds delay
+
+        # elif pnl <= -60 and not self.position_closed:
+        #     # self.textbox.append(f"Position closed with Loss {pnl}")
+        #     self.close_position_clicked()
+        #     self.textbox.append(f"Position closed with Loss {pnl}")
+        #     chosen_side = self.random_side_chooser()
+
+        #     QTimer.singleShot(10000, lambda: self.delayed_order(chosen_side))  # 10 seconds delay
+
+
+    def init_session(self, key, secret, testnet):
+        # Initialize the trading session with the provided API key, secret, and testnet flag
+        try:
+            return HTTP(api_key=key, api_secret=secret, testnet=testnet)
+        except Exception as e:
+            print(f"here {e}")
+
+    def init_exchange(self, key, secret):
+        # Initialize the exchange object (ccxt) with the provided API key and secret
+        exchange = ccxt.bybit({'apiKey': key, 'secret': secret, "options": {'defaultType': 'future'}})
+        exchange.set_sandbox_mode(True)  # Set sandbox mode for testing
+        return exchange
+
+    def initUI(self):
+        # Initialize the user interface (UI) layout
+        layout = QVBoxLayout()
+        button_layout = QHBoxLayout()
+        pnl_balance_layout = QVBoxLayout()
+        buttons = [("Buy", self.buy_clicked), ("Sell", self.sell_clicked)]
+        self.textbox = QTextEdit(self)
+        self.textbox.setReadOnly(True)
+
+        # Create labels for PNL and account balance
+        self.pos_side = QLabel(self)
+        self.pnl_label = QLabel(self)
+        # self.true_pnl_label = QLabel(self)
+        self.balance_label = QLabel(self)
+        
+        pnl_balance_layout.addWidget(self.pos_side)
+        pnl_balance_layout.addWidget(self.pnl_label)
+        pnl_balance_layout.addWidget(self.balance_label)
+
+        for name, action in buttons:
+            btn = QPushButton(name, self)
+            btn.clicked.connect(action)
+            button_layout.addWidget(btn)
+
+        # Create and add the Close Position button
+        close_position_btn = QPushButton("Close Position", self)
+        close_position_btn.clicked.connect(self.close_position_clicked)
+
+        layout.addLayout(button_layout)
+        layout.addWidget(self.textbox)
+        layout.addLayout(pnl_balance_layout)
+        layout.addWidget(close_position_btn)  # Add the button to the layout
+
+        self.setLayout(layout)
+        self.setWindowTitle('Auto Trading App')
+        self.show()
+    
+    def buy_clicked(self):
+        self.position_closed = False
+        self.clicked("Buy")
+        
+
+    def sell_clicked(self):
+        self.position_closed = False
+        self.clicked("Sell")
+
+    def clicked(self, side):
+        ticker_price = self.get_ticker_price()
+        available_balance = self.get_available_balance()
+        order_quantity = self.calculate_order_quantity(available_balance, ticker_price, leverage)
+        print(order_quantity)
+        self.set_leverage()
+
+        self.place_order(order_quantity, side)
+        my_entry = self.get_entry_price()
+        position_qty = self.get_position_quantity()
+
+        timestamp = QDateTime.currentDateTime().toString("yyyy-MMM-dd hh:mm:ss ")
+
+        self.set_take_profit_stop_loss(position_qty, my_entry, side)
+
+        if side == "Buy":
+            self.textbox.append("<font face='Roboto' color='green'>{1} <b>| {0} </b></font>".format(side, timestamp))
+            # self.textbox.append(f"Take Profit: {self.myTakeProfit}")
+            # self.textbox.append(f"Stop Loss: {self.myStopLoss}")
+            # self.textbox.append(f"Entry Price: {my_entry}") 
+            # pass     
+        elif side == "Close":
+            self.textbox.append("<font face='Roboto' color='#FFAA00'>{1} <b>| {0} </b></font>".format(side, timestamp))
+        elif side == "Sell":
+            self.textbox.append("<font face='Roboto' color='red'>{1} <b>| {0} </b></font>".format(side, timestamp))
+            # self.textbox.append(f"Take Profit: {self.myTakeProfit}")
+            # self.textbox.append(f"Stop Loss: {self.myStopLoss}")
+            # self.textbox.append(f"Entry Price: {my_entry}")
+
+    
+    def close_position_clicked(self):
+        self.position_closed = True
+        data = self.session.get_positions(category="linear",symbol=symbol)
+        Qty = data['result']['list'][0]['size']
+        position_side = data['result']['list'][0]['side']
+        if position_side != "None":
+            self.clicked("Close")
+            
+            # print(Qty)
+            # exchange.create_order(symbol, amount=Qty, type='Market', side='sell', params={'reduce_only': True})
+            self.session.place_order(category="linear", symbol=symbol, side=("Sell" if position_side == "Buy" else "Buy"),
+                                                orderType="Market", qty=Qty, reduceOnly=True,
+                                                timeInForce="PostOnly", positionIdx="0")
+            # self.textbox.append("Position closed.")
+        # else:
+        #     self.textbox.append("No Position To close")
+
+
+    def getSide(self, side):
+        if side == "Buy":
+            self.pos_side.setText(f"<font color='green'><b>Side:</b> {side}</font>")
+        else:
+            self.pos_side.setText(f"<font color='red'><b>Side:</b> {side}</font>")
+
+    def get_ticker_price(self):
+        # Get the current ticker price for BTC
+        btc_price = self.session.get_tickers(category="linear", symbol=symbol)
+        return float(btc_price['result']['list'][0]['lastPrice'])
+
+    def get_available_balance(self):
+        # Get the available balance in the account
+        balance = self.session.get_coins_balance(accountType="UNIFIED", coin="USDT")
+        return float(balance['result']['balance'][0]['transferBalance'])
+    
+    def calculate_decimal_quantity(self, num):
+        # Take the integer part of the number
+        integer_part = int(num)
+        
+        # Get the number of digits of the integer part
+        digit_count = len(str(integer_part))
+        
+        if digit_count == 5:
+            return 3
+        elif 2 <= digit_count <= 4:
+            return 2
+        elif integer_part == 0:
+            return 0
+        elif digit_count == 1:
+            return 1
+
+    def calculate_order_quantity(self, available_balance, ticker_price, leverage):
+        # Calculate the order quantity based on available balance, ticker price, and leverage
+        decimal_qty = self.calculate_decimal_quantity(ticker_price)
+        calculated_order_quantity = ((available_balance * int(leverage)) / ticker_price) * .9
+        calculated_order_quantity = round(calculated_order_quantity, decimal_qty)
+        # print(type(calculated_order_quantity))
+        return calculated_order_quantity
+    
+    def set_leverage(self):
+        # Set the leverage for trading
+        try:
+            self.session.set_leverage(category="linear", symbol=symbol, buyLeverage=leverage, sellLeverage=leverage)
+        except Exception as e:
+            # print(f"set leverage {e}")
+            pass
+
+    def place_order(self, order_quantity, side):
+        # Place a market order with the specified order quantity and side
+        # print(type(self.calculate_order_quantity()))
+        try:
+            self.set_margin_mode()
+            my_order = self.session.place_order(category="linear", symbol=symbol, side=side, orderType="Market",
+                                                qty=order_quantity)
+            return my_order['result']['orderId']
+        except Exception as e:
+            # self.textbox.append(f"place_order Failed: {e} /n")
+            print(e)
+            return None
+
+    def get_entry_price(self):
+        # Get the entry price of the current position
+        try:
+            data = self.session.get_positions(category="linear",symbol=self.symbol,)
+            entryPrice = data['result']['list'][0]['avgPrice']
+            return entryPrice
+        except Exception as e:
+            print(e)
+            return None
+
+    def get_position_quantity(self):
+        # Get the quantity of the current position
+        try:
+            data = self.session.get_positions(category="linear",symbol=self.symbol,)
+            Qty = data['result']['list'][0]['size']
+            return Qty
+        except Exception as e:
+            print(e)
+            return None
+        
+    def set_margin_mode(self):
+        return self.session.set_margin_mode(
+        setMarginMode="ISOLATED_MARGIN",)
+
+    def set_take_profit_stop_loss(self, position_qty, my_entry, side):
+        # Set the take profit and stop loss levels for the current position
+        if side == "Buy":
+            self.myTakeProfit = round(float(my_entry) * (1 + take_profit_percentage / 100), 2)
+            self.myStopLoss = round(float(my_entry) * (1 - stop_loss_percentage / 100), 2)
+        else:
+            self.myTakeProfit = round(float(my_entry) * (1 - take_profit_percentage / 100), 2)
+            self.myStopLoss = round(float(my_entry) * (1 + stop_loss_percentage / 100), 2)
+        
+
+        try:
+            # Set the trading stop (stop loss) level
+            self.session.set_trading_stop(category="linear", symbol=symbol, stopLoss=self.myStopLoss, positionIdx=0)
+
+            # Place a limit order for take profit
+            self.session.place_order(category="linear", symbol=symbol, side=("Sell" if side == "Buy" else "Buy"),
+                                     orderType="Limit", qty=position_qty, price=self.myTakeProfit, reduceOnly=True,
+                                     timeInForce="PostOnly", positionIdx="0")
+            print(f"{side} position placed")
+        except Exception as e:
+            print(f"settakeprofit error{e}")
+
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = MyApp("LUTr5ux8UK85oMM5Tj", "tSbq88myGBidovLvlIxAbDlbVpmRgdyYklBv")
+    ex.start_pnl_thread()
+    sys.exit(app.exec_())
